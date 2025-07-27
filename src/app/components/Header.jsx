3c17b42e8808a44fd59b47ab81ab7cba954366fd
Kotaro <ino.kota0602@gmail.com>
2025-07-27 14:16:46 +0900

    style: ヘッダーにボーダー書き足し

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="bg-orange-400 px-6 py-4 text-center text-lg font-bold text-white">
+    <header className="border-b-4 border-orange-400 bg-white px-6 py-7 text-center text-lg font-bold text-gray-700">
       ヘッダー
     </header>
   );
